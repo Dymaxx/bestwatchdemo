ec1bd935f583d4c46b03f0376c506b37651044f6
Dymax_x <remcosmurf1@gmail.com>
2024-09-11 22:26:19 +0200

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,50 +4,32 @@ import { Button } from "./ui/button";
 import Image from "next/image";
 import { faker } from "@faker-js/faker/locale/en_CA";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 export function Hero() {
   return (
-    <div className=" bg-[#0d0d0d] w-full rounded-3xl">
-      <div className=" w-full h-96  grid grid-cols-2 ">
-        <div className="px-10 pr-24  pt-16 pb-8  flex flex-col justify-between ">
-          <div className="flex flex-col gap-10">
-            <h1 className="text-6xl  ">
-              The <span className="font-bold">Soul en Conductor</span>
-            </h1>
-
-            <div className="flex gap-3 items-center">
-              <div className="flex flex-row gap-2 items-center">
-                {" "}
-                <FaImdb className="text-2xl text-yellow-400 " />
-                <p className="text-xs">7.8</p>
-              </div>
-              <div className="flex flex-row gap-2  items-center">
-                {" "}
-                <US title="United States" className="w-6" />
-                <p className="text-xs"> English</p>
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center gap-3">
-            <Button className="bg-red-600 px-7 rounded-2xl">Info</Button>
-          </div>
+    <div className="relative group ratio-16/9 h-[500px]  flex flex-row transition duration-500 ease-in-out rounded-xl">
+      <div className=" p-4  w-2/5 bg-black text-white rounded-l-xl flex flex-col px-6  relative group justify-center gap-10 ">
+        <div className="flex flex-col  w-full  gap-4 ">
+          <h1 className="text-6xl ">Deadpool & Wolverine</h1>
+          <p className="text-md xl:text-lg text-gray-400">Release date: 2024</p>
         </div>
 
-        <div className=" relative">
-          <Image
-            className="w-full h-full scale-110 blur-3xl opacity-50 rounded-2xl absolute"
-            src="/soul.png"
-            width={500}
-            height={500}
-            alt="Picture of the author"
-          />
-          <Image
-            className="w-full h-full rounded-lg absolute "
-            src="/soul.png"
-            width={500}
-            height={500}
-            alt="Picture of the author"
-          />
-        </div>
+        <Link
+          href="/details/movie/Deadpool%20&%20Wolverine"
+          className="px-2 py-2 w-18 rounded-lg flex text-center items-center justify-center text-md bg-primary text-white hover:bg-[#f00] transition duration-300 ease-in-out"
+        >
+          More info
+        </Link>
+      </div>
+      <div className="relative hidden h-full w-full md:flex object-portition-bottom">
+        <Image
+          className=" flex  w-full h-full rounded-r-2xl object-top object-cover"
+          src={"/images/hero.png"}
+          width={500}
+          height={500}
+          alt="Poster"
+        />
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black via-black/40 to-transparent rounded-r-2xl"></div>
       </div>
     </div>
   );
